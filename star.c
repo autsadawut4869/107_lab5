767fcfb89a5df63746fe1865de3e1eafbb18a696
autsadawut4869 <78350629+autsadawut4869@users.noreply.github.com>
2021-03-08 20:08:21 +0700

    edit

--- star.c
+++ star.c
@@ -1,24 +1,5 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    int n,i,j;
-    scanf("%d",&n);
-    int s=n-1,star=1;
-    for(i=1;i<=n;i++)
-    {
-        if(i==1)
-        {
-          for(j=n/2;j>=1;j--)
-            {
-                printf("_");
-            }
-        }
 
-        for(j=i;j<=n-1;j++)
-        {
-            printf("*");
-        }
-        printf("\n");
-      }
-    return 0;
 }
